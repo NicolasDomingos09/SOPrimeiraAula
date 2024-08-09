85f64654295a7ee17b8f9da212bf46b242ac3cc9
unknown <nicolasdomingos149@gmail.com>
2024-08-09 19:19:21 -0300

    exerício 3

--- Projeto03/src/controller/ClasseMetodos.java
+++ Projeto03/src/controller/ClasseMetodos.java
@@ -1,5 +1,33 @@
 package controller;
+import java.util.Scanner;
 
 public class ClasseMetodos {
-
+	
+	public ClasseMetodos() {
+		super();
+	}
+	
+	public int[] carregaVetor(int[] vetor) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Insira os valores inteiros para preencher o vetor");
+		int tamanho = vetor.length;
+		for(int i = 0; i < tamanho; i++) {
+			vetor[i] = scan.nextInt(); 
+		}
+		scan.close();
+		return vetor;
+	}
+	
+	public void percorreVetor(int[] vetor) {
+		for(int num : vetor) {
+			if(num % 2 == 0) {
+				if(num % 10 == 0) {
+					System.out.println("Número " + num + " é par e múltiplo de 10");
+				}
+			}else {
+				System.out.println("Número " + num + " é ímpar");
+			}
+		}
+	}
 }
--- Projeto03/src/view/Principal.java
+++ Projeto03/src/view/Principal.java
@@ -8,14 +8,21 @@ public class Principal {
 		Scanner scan = new Scanner(System.in);
 		ClasseMetodos met = new ClasseMetodos();
 		
-		boolean coleta = false;
-		while (!coleta) {
-			int tamanho = scan.nextInt();
-			if(tamanho <= 100 && tamanho > 0) {
-				coleta = true;
-			}
+		//Coleta do tamanho limitando a 100
+		int tamanho = 0;
+		while (tamanho <= 0 || tamanho > 100) {
+			System.out.println("Entre com um valor inteiro entre 0 e 100 para o tamanho do vetor");
+			tamanho = scan.nextInt();
 		}
+		
+		//Criação do vetor
+		int[] vetorInteiro = new int[tamanho];
+		
+		//Carrega vetor
+		vetorInteiro = met.carregaVetor(vetorInteiro);
+		
+		//Percorre vetor
+		met.percorreVetor(vetorInteiro);
 		scan.close();
-
 	}
 }
